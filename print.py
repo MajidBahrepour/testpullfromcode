14ce2afecf3ff1309ba8abf5cd70111c9efd07f9
Majid Bahrepour <majid.bahrepour@shell.com>
2023-05-05 09:40:08 +0000

    merged pull into print

--- print.py
+++ print.py
@@ -1,8 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
+import subprocess
 
+# Discard any local changes using git reset 
+subprocess.call(["git", "reset", "--hard"])
+
+# Define the git pull command to execute 
+command = ["git", "pull", "origin", "main"]
+
+# Execute the git pull command and capture the output 
+output = subprocess.check_output(command)
+
+# Print the output
+print(output.decode("utf-8"))
 
 import os
 
